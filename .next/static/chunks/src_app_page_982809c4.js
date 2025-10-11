3c1d4f2556c4369cf0694e471c93fe9de6e72dba
Tanmay Patil <dhhanaji7738@gmail.com>
2025-10-11 08:06:13 +0530

    Email error solved

--- .next/static/chunks/src_app_page_982809c4.js
+++ .next/static/chunks/src_app_page_982809c4.js
@@ -2,10 +2,9 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/src_8b961b33._.js",
-  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_56b6fd65._.js",
   "static/chunks/node_modules_lodash_92b97f43._.js",
   "static/chunks/node_modules_recharts_es6_984b87bb._.js",
-  "static/chunks/node_modules_927ecd8a._.js"
+  "static/chunks/node_modules_4d3f8241._.js"
 ],
     source: "dynamic"
 });
