6e2bf0d22c1339b5822ed5274c9f6ae7d36b5e0f
unknown <amityedage371@gmail.com>
2026-01-24 21:07:51 +0530

    Some files are deleted

--- fix_password_column.sql
+++
@@ -1,6 +0,0 @@
--- SQL script to fix the password column type in Supabase
--- Run this in your Supabase SQL editor
-
--- Option 1: If the table is empty or you don't mind losing data
-ALTER TABLE "Farmer Data" ALTER COLUMN password TYPE TEXT;
-
